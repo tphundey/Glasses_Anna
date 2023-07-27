df9984ec93bec7b5b47a0a8194598a3e3f423a58
Your Name <youremail@yourdomain.com>
2023-07-28 04:13:43 +0700

    style: create trangchu

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 // import ProductList from "./components/ProductList";
 import "react-loading-skeleton/dist/skeleton.css";
 import Header from "./components/header";
+import TrangChu from "./components/TrangChu"
 import "./App.css"
+import Footer from "./components/footer";
 function App() {
     return (
 
         <div>
             <Header></Header>
+            <TrangChu></TrangChu>
+            <Footer></Footer>
             {/* <ProductList /> */}
 
         </div>
---
+++ src/components/TrangChu/index.tsx
@@ -0,0 +1,84 @@
+import "./style.css"
+
+const Header = () => {
+
+    return (
+        <div>
+            <div className="banner">
+                <img src="https://kinhmatanna.com/wp-content/uploads/2023/02/Untitled-3-02-1-2048x665.png" alt="" />
+            </div>
+
+            <div className="container">
+                <div className="title">
+                    <div className="tl1">HOT TREND</div>
+                    <div className="tl2">HÀNG MỚI LÊN KỆ</div>
+                </div>
+                <div className="list">
+                    <div className="product">
+                        <div className="image">
+                            <img src="https://kinhmatanna.com/wp-content/uploads/2023/06/DSC_4094-copy-1-300x300.jpg" alt="" />
+                        </div>
+                        <div className="name">GK - 380CK116</div>
+                        <div className="price">342,000đ</div>
+                    </div>
+
+                    <div className="product">
+                        <div className="image">
+                            <img src="https://kinhmatanna.com/wp-content/uploads/2023/06/DSC_4094-copy-1-300x300.jpg" alt="" />
+                        </div>
+                        <div className="name">GK - 380CK116</div>
+                        <div className="price">342,000đ</div>
+                    </div>
+
+                    <div className="product">
+                        <div className="image">
+                            <img src="https://kinhmatanna.com/wp-content/uploads/2023/06/DSC_4094-copy-1-300x300.jpg" alt="" />
+                        </div>
+                        <div className="name">GK - 380CK116</div>
+                        <div className="price">342,000đ</div>
+                    </div>
+
+                    <div className="product">
+                        <div className="image">
+                            <img src="https://kinhmatanna.com/wp-content/uploads/2023/06/DSC_4094-copy-1-300x300.jpg" alt="" />
+                        </div>
+                        <div className="name">GK - 380CK116</div>
+                        <div className="price">342,000đ</div>
+                    </div>
+
+                    <div className="product">
+                        <div className="image">
+                            <img src="https://kinhmatanna.com/wp-content/uploads/2023/06/DSC_4094-copy-1-300x300.jpg" alt="" />
+                        </div>
+                        <div className="name">GK - 380CK116</div>
+                        <div className="price">342,000đ</div>
+                    </div>
+
+                    <div className="product">
+                        <div className="image">
+                            <img src="https://kinhmatanna.com/wp-content/uploads/2023/06/DSC_4094-copy-1-300x300.jpg" alt="" />
+                        </div>
+                        <div className="name">GK - 380CK116</div>
+                        <div className="price">342,000đ</div>
+                    </div>
+                </div>
+                <div className="title">
+                    <div className="tl1">ĐỊA ĐIỂM</div>
+                    <div className="tl2">HỆ THỐNG CỬA HÀNG</div>
+                </div>
+                <div className="diadiem">
+                    <div className="left">
+                        <img src="https://kinhmatanna.com/wp-content/uploads/2022/10/Untitled-1.png" alt="" />
+                    </div>
+                    <div className="right">
+                        <img src="https://kinhmatanna.com/wp-content/uploads/2022/10/756daa592822ed7cb433-768x1365.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Header;
